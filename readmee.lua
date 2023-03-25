00706e3989968c9f5ebb64fbe21d90f4f7987324
Simoniko <52357715+Simoniko@users.noreply.github.com>
2023-03-25 20:57:32 +0100

    Update readmee.lua

--- readmee.lua
+++ readmee.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Simoniko/roblox-user-thing/main/profile.html"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Simoniko/roblox-user-thing/main/profile.lua"))()
